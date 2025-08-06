e803c7999089561e535a5fb0820e9c50daf13ad2
Ben <Ben@Ben>
2025-08-06 16:15:43 -0600

    Commit 3-(Se actualizaron algunas cosas)

--- src/buscaminas/modelo/Tablero.java
+++ src/buscaminas/modelo/Tablero.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package minerojuego.vista;
+package buscaminas.modelo;
 
 /**
  *
  * @author pame
  */
-public class JuegoGUI {
+public class Tablero {
     
 }
--- src/buscaminas/util/Temporizador.java
+++ src/buscaminas/util/Temporizador.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package minerojuego.modelo;
+package buscaminas.util;
 
 /**
  *
  * @author pame
  */
-public class Juego {
+public class Temporizador {
     
 }
