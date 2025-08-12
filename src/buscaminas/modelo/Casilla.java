dcb1e2fca78c8586189893f406a54faf0b0dc496
pame0520 <pamelaespinozapotoy@gmail.com>
2025-08-11 22:57:35 -0600

    se agrego codigo en la clase casilla

--- src/buscaminas/modelo/Casilla.java
+++ src/buscaminas/modelo/Casilla.java
@@ -19,7 +19,47 @@ public class Casilla {
         this.posFila = posFila;
         this.posColumna = posColumna;
     }
+
+    public int getPosFila() {
+        return posFila;
+    }
+
+    public void setPosFila(int posFila) {
+        this.posFila = posFila;
+    }
+
+    public int getPosColumna() {
+        return posColumna;
+    }
+
+    public void setPosColumna(int posColumna) {
+        this.posColumna = posColumna;
+    }
+
+    public boolean isMina() {
+        return mina;
+    }
+
+    public void setMina(boolean mina) {
+        this.mina = mina;
+    }
+
+    public int getNumMinasAlrededor() {
+        return numMinasAlrededor;
+    }
+
+    public void setNumMinasAlrededor(int numMinasAlrededor) {
+        this.numMinasAlrededor = numMinasAlrededor;
+    }
+
+    public boolean isAbierta() {
+        return abierta;
+    }
+
+    public void setAbierta(boolean abierta) {
+        this.abierta = abierta;
+    }
     
-    //
+    
     
 }
