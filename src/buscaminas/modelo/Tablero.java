c37f282219f8c63cb267f6436cf87247a46cd4f6
pame0520 <pamelaespinozapotoy@gmail.com>
2025-08-11 23:00:41 -0600

    se agrego codigo en la clase Tablero

--- src/buscaminas/modelo/Tablero.java
+++ src/buscaminas/modelo/Tablero.java
@@ -4,6 +4,7 @@
  */
 package buscaminas.modelo;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -105,21 +106,109 @@ public class Tablero {
         }
     }*/
     
-    //
+    private List<Casilla> obtenerCasillasAlrededor(int posFila, int posColumna) {
+        List<Casilla> listaCasillas = new LinkedList<>();
+        for (int i = 0; i < 8; i++) {
+            int tmpPosFila = posFila;
+            int tmpPosColumna = posColumna;
+            switch (i) {
+                case 0:
+                    tmpPosFila--;
+                    break; //Arriba
+                case 1:
+                    tmpPosFila--;
+                    tmpPosColumna++;
+                    break; //Arriba Derecha
+                case 2:
+                    tmpPosColumna++;
+                    break; //Derecha
+                case 3:
+                    tmpPosColumna++;
+                    tmpPosFila++;
+                    break; //Derecha Abajo
+                case 4:
+                    tmpPosFila++;
+                    break; //Abajo
+                case 5:
+                    tmpPosFila++;
+                    tmpPosColumna--;
+                    break; //Abajo Izquierda
+                case 6:
+                    tmpPosColumna--;
+                    break; //Izquierda
+                case 7:
+                    tmpPosFila--;
+                    tmpPosColumna--;
+                    break; //Izquierda Arriba
+            }
+
+            if (tmpPosFila >= 0 && tmpPosFila < this.casillas.length
+                    && tmpPosColumna >= 0 && tmpPosColumna < this.casillas[0].length) {
+                listaCasillas.add(this.casillas[tmpPosFila][tmpPosColumna]);
+            }
+
+        }
+        return listaCasillas;
+    }
     
-    //
+     List<Casilla> obtenerCasillasConMinas() {
+        List<Casilla> casillasConMinas = new LinkedList<>();
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                if (casillas[i][j].isMina()) {
+                    casillasConMinas.add(casillas[i][j]);
+                }
+            }
+        }
+        return casillasConMinas;
+    }
     
-    //
-   
-    //
+    /*public void seleccionarCasilla(int posFila, int posColumna) {
+        if (!this.generacionMinas) {
+            this.generarMinas(posFila, posColumna);
+        }
+        eventoCasillaAbierta.accept(this.casillas[posFila][posColumna]);
+        if (this.casillas[posFila][posColumna].isMina()) {
+            eventoPartidaPerdida.accept(obtenerCasillasConMinas());
+        } else if (this.casillas[posFila][posColumna].getNumMinasAlrededor() == 0) {
+            marcarCasillaAbierta(posFila, posColumna);
+            List<Casilla> casillasAlrededor = obtenerCasillasAlrededor(posFila, posColumna);
+            for (Casilla casilla : casillasAlrededor) {
+                if (!casilla.isAbierta()) {
+                    seleccionarCasilla(casilla.getPosFila(), casilla.getPosColumna());
+                }
+            }
+        } else {
+            marcarCasillaAbierta(posFila, posColumna);
+        }
+        if (partidaGanada()) {
+            eventoPartidaGanada.accept(obtenerCasillasConMinas());
+        }
+    }*/
     
-    //
+    public void marcarCasillaAbierta(int posFila, int posColumna) {
+        if (!this.casillas[posFila][posColumna].isAbierta()) {
+            numCasillasAbiertas++;
+            this.casillas[posFila][posColumna].setAbierta(true);
+        }
+    }
     
-    //
-    
-    //
-    
-    //
+    boolean partidaGanada() {
+        return numCasillasAbiertas >= (numFilas * numColumnas) - numMinas;
+    }
+
+
+    public void setEventoPartidaPerdida(Consumer<List<Casilla>> eventoPartidaPerdida) {
+        this.eventoPartidaPerdida = eventoPartidaPerdida;
+    }
+
+    public void setEventoCasillaAbierta(Consumer<Casilla> eventoCasillaAbierta) {
+        this.eventoCasillaAbierta = eventoCasillaAbierta;
+    }
+
+    public void setEventoPartidaGanada(Consumer<List<Casilla>> eventoPartidaGanada) {
+        this.eventoPartidaGanada = eventoPartidaGanada;
+    }
     
     
 }
